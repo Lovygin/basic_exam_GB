8738aba35fe7a2fa4582c634597285445573ff18
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-06-29 00:59:36 +0300

    reduced array problem solved

--- Separate_string_array/Program.cs
+++ Separate_string_array/Program.cs
@@ -11,12 +11,27 @@ for (int i = 0; i < len; i++)
     WriteLine("Please, enter array content at position: " + position);//введите данные массива на позиции ...
     longArr[i] = ReadLine();
 }
-for (int i = 0; i < longArr.Length; i++)
-{
-    Write(longArr[i] + "\t");
-}//git commit -m 'array data entry problem solved' - решена задача ввода данных в массив
+
+        for (int i = 0; i < longArr.Length; i++)
+        {
+            Write(longArr[i] + "\t");
+        }//git commit -m 'array data entry problem solved' - решена задача ввода данных в массив
+
+WriteLine();
 if(longArr.Length <= 3)
 {
     string[] nullArr = new string[0];
     WriteLine("Array reduction is not requaried");//уменьшение массива не требуется
+}
+else
+{
+    string[] shortArr = new string[3];
+    Random random = new Random();//Необходима переменная random, так как метод Next типа Random - экземплярный метод. 
+    WriteLine("new short array: ");
+    for (int i = 0; i < 4; i++)
+    {
+        int index = random.Next(0,longArr.Length);
+        shortArr[i] = longArr[index];
+        Write(shortArr[i] + "\t");
+    }
 }
