9070bcba62e4eeef1b8826856e6e49c7f8e025fd
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-06-29 00:27:19 +0300

    для альтернативного решения создана новая ветка: alter

--- Separate_string_array/Program.cs
+++ Separate_string_array/Program.cs
@@ -2,16 +2,21 @@
 using static System.Console;
 
 Clear();
-WriteLine("Please, enter decimal numbers to determine the length of the new array");
+WriteLine("Please, enter decimal numbers to determine the length of the new array");//введите число для определения длины массива
 int len = int.Parse(ReadLine());
 string[] longArr = new string[len];
 for (int i = 0; i < len; i++)
 {
     int position = i + 1;
-    WriteLine("Please, enter array content at position: " + position);
+    WriteLine("Please, enter array content at position: " + position);//введите данные массива на позиции ...
     longArr[i] = ReadLine();
 }
 for (int i = 0; i < longArr.Length; i++)
 {
     Write(longArr[i] + "\t");
+}//git commit -m 'array data entry problem solved' - решена задача ввода данных в массив
+if(longArr.Length <= 3)
+{
+    string[] nullArr = new string[0];
+    WriteLine("Array reduction is not requaried");//уменьшение массива не требуется
 }
