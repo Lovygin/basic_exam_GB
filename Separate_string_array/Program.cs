0a15a32693d8ad71e2c9ff7eb4efc6a149118477
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-06-29 00:18:05 +0300

    array data entry problem solved

---
+++ Separate_string_array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Please, enter decimal numbers to determine the length of the new array");
+int len = int.Parse(ReadLine());
+string[] longArr = new string[len];
+for (int i = 0; i < len; i++)
+{
+    int position = i + 1;
+    WriteLine("Please, enter array content at position: " + position);
+    longArr[i] = ReadLine();
+}
+for (int i = 0; i < longArr.Length; i++)
+{
+    Write(longArr[i] + "\t");
+}
